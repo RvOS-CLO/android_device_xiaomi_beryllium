e29b4bb388e5acfd0633f594e3e01372019a79c3
Rve27 <rve27github@gmail.com>
2025-02-28 12:35:28 +0700

    beryllium: hidl => vintf
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,10 +48,10 @@ LOC_HIDL_VERSION := 4.0
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-     $(DEVICE_PATH)/configs/hidl/xiaomi_vendor_framework_compatibility_matrix.xml
+     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
-     $(DEVICE_PATH)/configs/hidl/manifest.xml
+     $(DEVICE_PATH)/configs/vintf/manifest.xml
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 1
